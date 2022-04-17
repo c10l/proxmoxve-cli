c69f46911533cb6c74cb7a01ac9f441553affbf7
c10l <git@c10l.cc>
2022-04-17 22:08:42 +0100

    Fix when url contains trailing /

--- cmd/client.go
+++ cmd/client.go
@@ -1,13 +1,15 @@
 package cmd
 
 import (
+	"strings"
+
 	"github.com/c10l/proxmoxve-client-go/api2"
 	"github.com/spf13/viper"
 )
 
 func newClient() *api2.Client {
 	return api2.NewClient(
-		viper.GetString("url"),
+		strings.Trim(viper.GetString("url"), "/"),
 		viper.GetString("token-id"),
 		viper.GetString("secret"),
 		viper.GetBool("insecure"))
